aceb68dfaea5b04065a478240989a35ab6151808
Robert <109656109+RobertTwardowski@users.noreply.github.com>
2022-11-07 14:43:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,135 +1,42 @@
-const w = document.querySelector(".w");
-const a = document.querySelector(".a");
-const s = document.querySelector(".s");
-const d = document.querySelector(".d");
-const j = document.querySelector(".j");
-const k = document.querySelector(".k");
-const l = document.querySelector(".l");
+const set = document.querySelectorAll("button");
 
-const crash = () => {
-  const crash = new Audio("sounds/crash.mp3");
-  crash.play();
-  wAdd();
+const click = (e) => {
+  let userClickedPattern = e.target.id;
+  let audio = new Audio(`sounds/${userClickedPattern}.mp3`);
+  audio.play();
+  className(e);
 };
-const wAdd = () => {
-  w.classList.add("pressed");
+const className = (e) => {
+  let className = e.target.className;
+  e.target.setAttribute("class", `${className} pressed`);
+
   setTimeout(() => {
-    w.classList.remove("pressed");
+    e.target.setAttribute("class", className);
   }, 100);
 };
-const kickBass = () => {
-  const kickBass = new Audio("sounds/kick-bass.mp3");
-  kickBass.play();
-  aAdd();
+
+const press = (e) => {
+  userClickedPattern = e.keyCode;
+
+  set.forEach((e) => {
+    let name = [e.id, e.className];
+    let className = name.slice(1);
+    let sliceName = String(className);
+    if (sliceName.slice(0, 1) === String.fromCharCode(userClickedPattern)) {
+      let sound = String(name.slice(0, 1));
+      let audio = new Audio(`sounds/${sound}.mp3`);
+      audio.play();
+      e.classList.add("pressed");
+      setTimeout(() => {
+        e.classList.remove("pressed");
+      }, 100);
+    }
+  });
 };
-const aAdd = () => {
-  a.classList.add("pressed");
-  setTimeout(() => {
-    a.classList.remove("pressed");
-  }, 100);
-};
-const snare = () => {
-  const snare = new Audio("sounds/snare.mp3");
-  snare.play();
-  sAdd();
-};
-const sAdd = () => {
-  s.classList.add("pressed");
-  setTimeout(() => {
-    s.classList.remove("pressed");
-  }, 100);
-};
-const tomOne = () => {
-  const tomOne = new Audio("sounds/tom-1.mp3");
-  tomOne.play();
-  dAdd();
-};
-const dAdd = () => {
-  d.classList.add("pressed");
-  setTimeout(() => {
-    d.classList.remove("pressed");
-  }, 100);
-};
-const tomTwo = () => {
-  const tomTwo = new Audio("sounds/tom-2.mp3");
-  tomTwo.play();
-  jAdd();
-};
-const jAdd = () => {
-  j.classList.add("pressed");
-  setTimeout(() => {
-    j.classList.remove("pressed");
-  }, 100);
-};
-const tomThree = () => {
-  const tomThree = new Audio("sounds/tom-3.mp3");
-  tomThree.play();
-  kAdd();
-};
-const kAdd = () => {
-  k.classList.add("pressed");
-  setTimeout(() => {
-    k.classList.remove("pressed");
-  }, 100);
-};
-const tomFour = () => {
-  const tomFour = new Audio("sounds/tom-4.mp3");
-  tomFour.play();
-};
-const lAdd = () => {
-  l.classList.add("pressed");
-  setTimeout(() => {
-    l.classList.remove("pressed");
-  }, 100);
-};
-document.addEventListener("keypress", (e) => {
-  if (e.keyCode == 119) {
-    crash();
-    wAdd();
-  }
+
+set.forEach((btn) => {
+  btn.addEventListener("click", click);
 });
 
-document.addEventListener("keypress", (e) => {
-  if (e.keyCode == 97) {
-    kickBass();
-    aAdd();
-  }
-});
-document.addEventListener("keypress", (e) => {
-  if (e.keyCode == 115) {
-    snare();
-    sAdd();
-  }
-});
-document.addEventListener("keypress", (e) => {
-  if (e.keyCode == 100) {
-    tomOne();
-    dAdd();
-  }
-});
-document.addEventListener("keypress", (e) => {
-  if (e.keyCode == 106) {
-    tomTwo();
-    jAdd();
-  }
-});
-document.addEventListener("keypress", (e) => {
-  if (e.keyCode == 107) {
-    tomThree();
-    kAdd();
-  }
-});
-document.addEventListener("keypress", (e) => {
-  if (e.keyCode == 108) {
-    tomFour();
-    lAdd();
-  }
-});
+document.addEventListener("keypress", press);
 
-w.addEventListener("click", crash);
-a.addEventListener("click", kickBass);
-s.addEventListener("click", snare);
-d.addEventListener("click", tomOne);
-j.addEventListener("click", tomTwo);
-k.addEventListener("click", tomThree);
-l.addEventListener("click", tomFour);
